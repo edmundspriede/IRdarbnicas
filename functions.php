d60677d3e52a0de17cc74764ce3922dc9d156864
Edmunds Priede <edmunds@kls.lv>
2023-07-12 09:54:19 +0300

    Update functions.php
    specifiski filtri upsell nodrošināšanai

--- functions.php
+++ functions.php
@@ -1,220 +1,96 @@
 <?php
-/**
- * Theme functions and definitions
- *
- * @package HelloElementor
- */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+//EP leibli priekš upsell checkbox uz jetformbuilder formas
+
+add_filter( 'jet-forms-generate-from-query/label'  , 'modify_label_for_upsells' , 10, 3);
+
+
+function modify_label_for_upsells($label, $obj, $args) {
+	
+	  //EP nolasam produktu un attēlojam datus
+          	
+          $product = wc_get_product( $obj->ID );
+          $price = $product->get_price() ;
+          $title = $product->get_title();
+          
+	  return $title.'  '.$price.' EUR';
 }
 
-define( 'HELLO_ELEMENTOR_VERSION', '2.6.1' );
 
-if ( ! isset( $content_width ) ) {
-	$content_width = 800; // Pixels.
+//EP pievienojam grozam visus produktus no upsell formas
+
+add_filter("jet-form-builder/action/redirect_to_woo_checkout/add-to-cart", 'formbuilder_add_to_cart' , 20 , 5);    
+
+
+function formbuilder_add_to_cart($params) {
+    
+    $cartdata['heading'] = 'heading';
+    $cartdata[] = array(
+							'key'     => 'Dalībnieki',
+							'display' => 'Dalībnieki'
+        );
+    
+    
+   
+    
+    //EP pievienojam galveno produktu
+    
+    WC()->cart->add_to_cart($params[0], $params[4]['jfb_form_data']['skaits'] , 0, array(),$params[4]); 
+
+    //EP pievienojam upsell produktus
+    
+    if ($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus']) {
+        
+        foreach($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus'] as $v) {
+         
+            
+                WC()->cart->add_to_cart($v, $params[4]['jfb_form_data']['skaits'] , 0, array(), $params[4]); 
+        }
+    }
+    
+    
+    return array();
+
 }
 
-if ( ! function_exists( 'hello_elementor_setup' ) ) {
-	/**
-	 * Set up theme support.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_setup() {
-		if ( is_admin() ) {
-			hello_maybe_update_theme_version_in_db();
-		}
 
-		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_load_textdomain', [ true ], '2.0', 'hello_elementor_load_textdomain' );
-		if ( apply_filters( 'hello_elementor_load_textdomain', $hook_result ) ) {
-			load_theme_textdomain( 'hello-elementor', get_template_directory() . '/languages' );
-		}
 
-		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_register_menus', [ true ], '2.0', 'hello_elementor_register_menus' );
-		if ( apply_filters( 'hello_elementor_register_menus', $hook_result ) ) {
-			register_nav_menus( [ 'menu-1' => __( 'Header', 'hello-elementor' ) ] );
-			register_nav_menus( [ 'menu-2' => __( 'Footer', 'hello-elementor' ) ] );
-		}
+//EP izvēlamies lielāko cenu variable produktam
 
-		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_add_theme_support', [ true ], '2.0', 'hello_elementor_add_theme_support' );
-		if ( apply_filters( 'hello_elementor_add_theme_support', $hook_result ) ) {
-			add_theme_support( 'post-thumbnails' );
-			add_theme_support( 'automatic-feed-links' );
-			add_theme_support( 'title-tag' );
-			add_theme_support(
-				'html5',
-				[
-					'search-form',
-					'comment-form',
-					'comment-list',
-					'gallery',
-					'caption',
-					'script',
-					'style',
-				]
-			);
-			add_theme_support(
-				'custom-logo',
-				[
-					'height'      => 100,
-					'width'       => 350,
-					'flex-height' => true,
-					'flex-width'  => true,
-				]
-			);
+add_filter( 'woocommerce_get_price_html', 'change_variable_price_display', 10, 2 );
 
-			/*
-			 * Editor Style.
-			 */
-			add_editor_style( 'classic-editor.css' );
+function change_variable_price_display( $price, $product_obj ) {
+    global $product;
 
-			/*
-			 * Gutenberg wide images.
-			 */
-			add_theme_support( 'align-wide' );
+    if ( 'variable' !== $product->get_type() || 'product_variation' === $product_obj->post_type ) {
+        return $price;
+    }
 
-			/*
-			 * WooCommerce.
-			 */
-			$hook_result = apply_filters_deprecated( 'elementor_hello_theme_add_woocommerce_support', [ true ], '2.0', 'hello_elementor_add_woocommerce_support' );
-			if ( apply_filters( 'hello_elementor_add_woocommerce_support', $hook_result ) ) {
-				// WooCommerce in general.
-				add_theme_support( 'woocommerce' );
-				// Enabling WooCommerce product gallery features (are off by default since WC 3.0.0).
-				// zoom.
-				add_theme_support( 'wc-product-gallery-zoom' );
-				// lightbox.
-				add_theme_support( 'wc-product-gallery-lightbox' );
-				// swipe.
-				add_theme_support( 'wc-product-gallery-slider' );
-			}
-		}
-	}
-}
-add_action( 'after_setup_theme', 'hello_elementor_setup' );
+    $price =  $product->get_variation_price( 'max', true ) ;
+    // Translators: %s is the lowest variation price.
+    $price =  wc_price( $price);
 
-function hello_maybe_update_theme_version_in_db() {
-	$theme_version_option_name = 'hello_theme_version';
-	// The theme version saved in the database.
-	$hello_theme_db_version = get_option( $theme_version_option_name );
-
-	// If the 'hello_theme_version' option does not exist in the DB, or the version needs to be updated, do the update.
-	if ( ! $hello_theme_db_version || version_compare( $hello_theme_db_version, HELLO_ELEMENTOR_VERSION, '<' ) ) {
-		update_option( $theme_version_option_name, HELLO_ELEMENTOR_VERSION );
-	}
+    return $price;
 }
 
-if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
-	/**
-	 * Theme Scripts & Styles.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_scripts_styles() {
-		$enqueue_basic_style = apply_filters_deprecated( 'elementor_hello_theme_enqueue_style', [ true ], '2.0', 'hello_elementor_enqueue_style' );
-		$min_suffix          = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+//EP pievienojam VAT lauku juridiskām personām
 
-		if ( apply_filters( 'hello_elementor_enqueue_style', $enqueue_basic_style ) ) {
-			wp_enqueue_style(
-				'hello-elementor',
-				get_template_directory_uri() . '/style' . $min_suffix . '.css',
-				[],
-				HELLO_ELEMENTOR_VERSION
-			);
-		}
+add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
 
-		if ( apply_filters( 'hello_elementor_enqueue_theme_style', true ) ) {
-			wp_enqueue_style(
-				'hello-elementor-theme-style',
-				get_template_directory_uri() . '/theme' . $min_suffix . '.css',
-				[],
-				HELLO_ELEMENTOR_VERSION
-			);
-		}
-	}
-}
-add_action( 'wp_enqueue_scripts', 'hello_elementor_scripts_styles' );
+function custom_override_checkout_fields($fields) {
 
-if ( ! function_exists( 'hello_elementor_register_elementor_locations' ) ) {
-	/**
-	 * Register Elementor Locations.
-	 *
-	 * @param ElementorPro\Modules\ThemeBuilder\Classes\Locations_Manager $elementor_theme_manager theme manager.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_register_elementor_locations( $elementor_theme_manager ) {
-		$hook_result = apply_filters_deprecated( 'elementor_hello_theme_register_elementor_locations', [ true ], '2.0', 'hello_elementor_register_elementor_locations' );
-		if ( apply_filters( 'hello_elementor_register_elementor_locations', $hook_result ) ) {
-			$elementor_theme_manager->register_all_core_location();
-		}
-	}
-}
-add_action( 'elementor/theme/register_locations', 'hello_elementor_register_elementor_locations' );
+  // $billing_reg = ['billing_regnr' => [ 'placeholder'  => 'LV************' , 'label' => 'Reģistrācijas Numurs(PVN)' , 'required' => false ]];
+      
+  // array_($fields['billing'], $billing_reg);
 
-if ( ! function_exists( 'hello_elementor_content_width' ) ) {
-	/**
-	 * Set default content width.
-	 *
-	 * @return void
-	 */
-	function hello_elementor_content_width() {
-		$GLOBALS['content_width'] = apply_filters( 'hello_elementor_content_width', 800 );
-	}
-}
-add_action( 'after_setup_theme', 'hello_elementor_content_width', 0 );
-
-if ( is_admin() ) {
-	require get_template_directory() . '/includes/admin-functions.php';
+   //print_r($fields);
+   //exit();
+   
+   return $fields;
 }
 
-/**
- * If Elementor is installed and active, we can load the Elementor-specific Settings & Features
-*/
 
-// Allow active/inactive via the Experiments
-require get_template_directory() . '/includes/elementor-functions.php';
-
-/**
- * Include customizer registration functions
-*/
-function hello_register_customizer_functions() {
-	if ( is_customize_preview() ) {
-		require get_template_directory() . '/includes/customizer-functions.php';
-	}
-}
-add_action( 'init', 'hello_register_customizer_functions' );
-
-if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
-	/**
-	 * Check hide title.
-	 *
-	 * @param bool $val default value.
-	 *
-	 * @return bool
-	 */
-	function hello_elementor_check_hide_title( $val ) {
-		if ( defined( 'ELEMENTOR_VERSION' ) ) {
-			$current_doc = Elementor\Plugin::instance()->documents->get( get_the_ID() );
-			if ( $current_doc && 'yes' === $current_doc->get_settings( 'hide_title' ) ) {
-				$val = false;
-			}
-		}
-		return $val;
-	}
-}
-add_filter( 'hello_elementor_page_title', 'hello_elementor_check_hide_title' );
-
-/**
- * Wrapper function to deal with backwards compatibility.
- */
-if ( ! function_exists( 'hello_elementor_body_open' ) ) {
-	function hello_elementor_body_open() {
-		if ( function_exists( 'wp_body_open' ) ) {
-			wp_body_open();
-		} else {
-			do_action( 'wp_body_open' );
-		}
-	}
-}
+define('JET_ENGINE_BREAK_BY_FIELD' , true);
+define('JET_ENGINE_BREAK_BY_QUERY_ID' , 'break_months');
+define( 'JET_ENGINE_BREAK_BY_FIELD', 'datums' );
+define('JET_ENGINE_BREAK_MONTH_FORMAT ' , 'M.Y');
