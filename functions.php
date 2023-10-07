16eb1ed091f427d3790b75e8bec3f31ed561932d
Edmunds Priede <edmunds@kls.lv>
2023-10-07 09:18:53 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,39 @@
 <?php
+/**
+ * Theme functions and definitions.
+ *
+ * For additional information on potential customization options,
+ * read the developers' documentation:
+ *
+ * https://developers.elementor.com/docs/hello-elementor-theme/
+ *
+ * @package HelloElementorChild
+ */
 
-//EP leibli priekš upsell checkbox uz jetformbuilder formas
-
-add_filter( 'jet-forms-generate-from-query/label'  , 'modify_label_for_upsells' , 10, 3);
-
-
-function modify_label_for_upsells($label, $obj, $args) {
-	
-	  //EP nolasam produktu un attēlojam datus
-          	
-          $product = wc_get_product( $obj->ID );
-          $price = $product->get_price() ;
-          $title = $product->get_title();
-          
-	  return $title.'  '.$price.' EUR';
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
 
+/**
+ * Load child theme scripts & styles.
+ *
+ * @return void
+ */
+function hello_elementor_child_scripts_styles() {
+
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
 //EP pievienojam grozam visus produktus no upsell formas
 
 add_filter("jet-form-builder/action/redirect_to_woo_checkout/add-to-cart", 'formbuilder_add_to_cart' , 20 , 5);    
@@ -24,74 +41,66 @@ add_filter("jet-form-builder/action/redirect_to_woo_checkout/add-to-cart", 'form
 
 function formbuilder_add_to_cart($params) {
     
-    $cartdata['heading'] = 'heading';
-    $cartdata[] = array(
-							'key'     => 'Dalībnieki',
-							'display' => 'Dalībnieki'
-        );
-    
+ 
+    if (isset($params[4]['jfb_form_data']['dalibnieku_dati'])) {
+        
+             $cartdata['dalibnieki_serial'] = serialize( $params[4]['jfb_form_data']['dalibnieku_dati']);
+             
+             $dalibnieki = '';
+             foreach($params[4]['jfb_form_data']['dalibnieku_dati'] as $k => $v) {
+             
+                   $dalibnieki .= implode(' ; ' , $v)."<br />";
+             }
+             
+             $cartdata['dalibnieki'] = $dalibnieki;
+    } 
     
    
     
     //EP pievienojam galveno produktu
     
-    WC()->cart->add_to_cart($params[0], $params[4]['jfb_form_data']['skaits'] , 0, array(),$params[4]); 
+    WC()->cart->add_to_cart($params[0], $params[4]['jfb_form_data']['skaits'] , 0, array(),$cartdata); 
 
     //EP pievienojam upsell produktus
     
     if ($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus']) {
         
-        foreach($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus'] as $v) {
+        if (is_array($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus'])) {
+      
+            foreach($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus'] as $v) {
          
-            
-                WC()->cart->add_to_cart($v, $params[4]['jfb_form_data']['skaits'] , 0, array(), $params[4]); 
-        }
-    }
+                            WC()->cart->add_to_cart($v, $params[4]['jfb_form_data']['skaits'] , 0, array(), $cartdata); 
+            }  
+        } else {
+          
+                WC()->cart->add_to_cart($params[4]['jfb_form_data']['pievieno_citus_cikla_seminarus'], $params[4]['jfb_form_data']['skaits'] , 0, array(), $cartdata); 
+        }  
+      }
     
     
     return array();
 
 }
 
-
-
-//EP izvēlamies lielāko cenu variable produktam
-
-add_filter( 'woocommerce_get_price_html', 'change_variable_price_display', 10, 2 );
-
-function change_variable_price_display( $price, $product_obj ) {
-    global $product;
-
-    if ( 'variable' !== $product->get_type() || 'product_variation' === $product_obj->post_type ) {
-        return $price;
-    }
-
-    $price =  $product->get_variation_price( 'max', true ) ;
-    // Translators: %s is the lowest variation price.
-    $price =  wc_price( $price);
-
-    return $price;
+function formbuilder_get_item_data( $item_data, $cart_item_data ) {
+ if( isset( $cart_item_data['dalibnieki'] ) ) {
+ $item_data[] = array(
+ 'key' => 'Dalībnieki',
+ 'value' =>  $cart_item_data['dalibnieki'] 
+ );
+ }
+ return $item_data;
 }
+add_filter( 'woocommerce_get_item_data', 'formbuilder_get_item_data', 10, 2 );
 
-//EP pievienojam VAT lauku juridiskām personām
 
-add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
-
-function custom_override_checkout_fields($fields) {
-
-  // $billing_reg = ['billing_regnr' => [ 'placeholder'  => 'LV************' , 'label' => 'Reģistrācijas Numurs(PVN)' , 'required' => false ]];
-      
-  // array_($fields['billing'], $billing_reg);
-
-   //print_r($fields);
-   //exit();
-   
-   return $fields;
+function formbuilder_checkout_create_order_line_item( $item, $cart_item_key, $values, $order ) {
+ if( isset( $values['dalibnieki_serial'] ) ) {
+ $item->add_meta_data(
+    'Dalībnieki' ,
+    $values['dalibnieki_serial'],
+ true
+ );
+ }
 }
-
-//EP month-breaker settingi
-
-define('JET_ENGINE_BREAK_BY_FIELD' , true);
-define('JET_ENGINE_BREAK_BY_QUERY_ID' , 'break_months');
-define( 'JET_ENGINE_BREAK_BY_FIELD', 'datums' );
-define('JET_ENGINE_BREAK_MONTH_FORMAT ' , 'M.Y');
+add_action( 'woocommerce_checkout_create_order_line_item', 'formbuilder_checkout_create_order_line_item', 10, 4 );
